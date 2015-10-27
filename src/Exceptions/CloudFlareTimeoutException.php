36f797d140ecd6fd6a0ce62f08b47f3f5d2f49be
Adam Weingarten <aweingarten@users.noreply.github.com>
2015-10-27 14:23:49 -0400

    Added CloudFlareTimeoutException to codebase.

---
+++ src/Exceptions/CloudFlareTimeoutException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Contains CloudFlareTimeoutException.
+ */
+
+namespace CloudFlarePhpSdk\Exceptions;
+
+/**
+ * Defines CloudFlareTimeoutException.
+ *
+ * Basic exception thrown from the CloudFlarePhpSdk.
+ */
+class CloudFlareTimeoutException extends CloudFlareException {}
